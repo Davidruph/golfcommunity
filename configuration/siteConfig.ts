f218f44064de287a642813e3b1e8b3e4009e6a1f
Agbugba David Chinweuba <57929456+Davidruph@users.noreply.github.com>
2026-01-27 14:36:26 +0100

    change to prod server

--- configuration/siteConfig.ts
+++ configuration/siteConfig.ts
@@ -1,13 +1,13 @@
 // enviroments
 const environment = {
   production: {
-    API_BASE_URL: 'https://api.golfcommunity.app/',
+    API_BASE_URL: 'https://golfcommunity-plum.vercel.app/api/v1',
   },
   development: {
     API_BASE_URL: 'http://localhost:3000/api/v1',
   },
 }
 
-const currentEnvironment = 'development'
+const currentEnvironment = 'production'
 
 export default environment[currentEnvironment]
